de4092748159f738b090311f6f9bd18ea19bb143
Jugarcia15 <Jugarcia15@outlook.com>
2023-05-30 14:46:57 -0500

    Update

---
+++ src/main/java/com/fullstack/FullStackProject/model/User.java
@@ -0,0 +1,24 @@
+package com.fullstack.FullStackProject.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    // Constructors, getters, and setters
+}
